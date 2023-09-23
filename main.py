3fe4f136a161152f48415c2b00321ea3a16cd162
Alberto27-git <ortiz_lopezalberto@outlook.com>
2023-09-22 20:21:37 -0500

    Actualización del código con una implementación completa en objetos

--- main.py
+++ main.py
@@ -6,35 +6,26 @@ Alberto Ortíz López
 import requests
 from bs4 import BeautifulSoup
 import time
+import math
 from random import randint, random
 
-megabit_bytes = 125000
+umbral = 8
+rango_max = 20
 
-class Canal:
-    def __init__(self, data, ruido_EM, prob_perdida, velocidad):
-        self.data = data
-        self.ruido_EM = ruido_EM
-        self.prob_perdida = prob_perdida
-        self.velocidad = velocidad
+class Fuente_informacion:
+    def __init__(self, website):
+        self.website = website
     
-    def envia_receptor(self, paquete):
-        print("Recibiendo...")
-        #Probabilidad de corromper los datos por ruido electromagnético
-        if self.ruido_EM:
-            num_rand_prob = randint(0, len(self.prob_perdida)-1 )
-            if self.prob_perdida[num_rand_prob] % 2 == 0:
-                paquete.header = False
-        
-        #Envia el paquete con o sin ruido dependiendo de la probabilidad anterior
-        receptor_1.decodifica_paquete(paquete)
-        print("STORAGE: ", len(receptor_1.storage))
-        time.sleep(1)
-
-class Paquete:
-    def __init__(self, header, data,  tail):
-        self.header = header
-        self.data = data
-        self.tail = tail
+    def descarga_info(self):
+        try:
+            response = requests.get(self.website)
+            response.raise_for_status()  # Verificar si la solicitud fue exitosa
+            return response.text        
+    
+        except Exception as e:
+            #Enviar excepción por el Transmisor
+            error = f'Error: {e}'
+            return error
 
 class Receptor:
     def __init__(self, storage):
@@ -43,62 +34,116 @@ class Receptor:
     def decodifica_paquete(self, paquete):
         if paquete.header and paquete.tail:
             caracteres = [chr(int(byte, 2)) for byte in paquete.data]
-            receptor_1.storage += ''.join(caracteres)
+            self.storage += ''.join(caracteres)
+
+class Transmisor:
+    def __init__(self, contenido):
+        self.contenido = contenido
+
+    def transmite(self, receptor, canal, entropia):
+        contenido_bin =  ' '.join(format(ord(caracter), '08b') for caracter in self.contenido)
+        bin_paquetes = [byte for byte in contenido_bin.split(' ')]
+        
+        while bin_paquetes:
+            segmento = Paquete(True,bin_paquetes[:canal.velocidad],True)
+            del bin_paquetes[:canal.velocidad]
+            #Empezar a transmitir por el canal en paquetes segun la velocidad
+            #la velocidad es la cantidad de bytes que se envian en un segundo en este caso
+            #125000 bytes que son 1 megabits
+            canal.envia_receptor(receptor, entropia, segmento)
+
+class Entropia:
+    def __init__(self, total_paquetes_enviados, entropia_final, registro_ruido):
+        self.total_paquetes_enviados = total_paquetes_enviados
+        self.entropia_final = entropia_final
+        self.registro_ruido = registro_ruido
+    
+    def calcula_entropia(self):
+        print(self.total_paquetes_enviados, "Probabilidad de pérdida")
+        for i in range(0,len(self.registro_ruido)):
+            self.entropia_final += (1/self.total_paquetes_enviados) * math.log2(1/self.total_paquetes_enviados)
+            
+        print("Entropia:", -self.entropia_final, )
+        print("Veces que ocurrió ruido: ", len(self.registro_ruido))
+        #calcular con las veces que se enviaron paquetes 
+        # y la cantidad de veces que cayó ruido
+
+class Canal:
+    def __init__(self, data, velocidad):
+        
+        self.data = data
+        self.velocidad = velocidad
+    
+    def envia_receptor(self, receptor, entropia, paquete):
+        entropia.total_paquetes_enviados += 1
+        print("Recibiendo...")
+        #Probabilidad de perder un paquete por ruido electromagnético
+        if self.ruido():
+            entropia.registro_ruido.append(True)
+            paquete.header = False
+    
+        #Envia el paquete con o sin ruido dependiendo de la probabilidad anterior
+        receptor.decodifica_paquete(paquete)
+        print("Tamaño de paquete recibido: ", len(receptor.storage))
+        time.sleep(1)
+    
+    def ruido(self):
+        num_aleatorio = randint(9,rango_max)
+        #Probabilidad del 40% de caer en el rango del umbral
+        #Número del 1 al 8
+        if num_aleatorio <= umbral:
+            return True
+
+class Paquete:
+    def __init__(self, header, data,  tail):
+        self.header = header
+        self.data = data
+        self.tail = tail
+
+class Destino:
+    def __init__(self, html):
+        self.html = html
+
+    def muestra_transmision(self):
+        soup = BeautifulSoup(self.html, 'lxml')
+        print(soup.prettify())
 
 def crea_canal():
-    prob_perdida = [2,4,5,7,8]
-
-    vel_actual = 10 - randint(4,9)
-    vel_actual *= megabit_bytes
-
-    prob_em = randint(1,10)
-    if prob_em % 2 == 0:
-        return Canal(0, True, prob_perdida, vel_actual)
-    else:
-        return Canal(0, False, prob_perdida, vel_actual)
-
-def Fuente_informacion(website):
-    try:
-        response = requests.get(website)
-        response.raise_for_status()  # Verificar si la solicitud fue exitosa
-        return response.text        
     
-    except Exception as e:
-        #Enviar excepción por el Transmisor
-        error = f'Error: {e}'
-        return error
-    
-def Transmisor(contenido):
-    contenido_bin =  ' '.join(format(ord(caracter), '08b') for caracter in contenido)
-    bin_paquetes = [byte for byte in contenido_bin.split(' ')]
-    
-    while bin_paquetes:
-        segmento = Paquete(True,bin_paquetes[:canal_1.velocidad],True)
-        del bin_paquetes[:canal_1.velocidad]
-        #Empezar a transmitir por el canal en paquetes segun la velocidad
-        canal_1.envia_receptor(segmento)
-
-def Destino(html):
-    soup = BeautifulSoup(html, 'lxml')
-    print(soup.prettify())
-
-canal_1 = crea_canal()
-receptor_1 = Receptor('')
+    megabit_bytes = 125000 #Cuanto equivale un megabit en bytes
+    vel_actual = 1
+    vel_actual = int(vel_actual * megabit_bytes)    
+    return Canal(None, vel_actual)
 
 def main():
-    #website = "https://es.wikipedia.org/wiki/Enciclopedia" # 183,511
-    #website = "https://es.wikipedia.org/wiki/Democracia" # 316,491
-    website = "https://es.wikipedia.org/wiki/Wikipedia" # 648,469
-    #https://www.microsoft.com/es-mx/
+    #website = "https://www.alamosinn.com/" # 918,339
+    #website = "https://www.alamosinn.com/explore" # 947,070
+    #website = "https://www.alamosinn.com/photo-albums" # 1,204,958
+    website = "https://www.frikidelto.com/tutorial/" # 1,204,998
+    
 
     #website = input("Ingresa url: ")
 
-    print("Buscando en la web...\n")
     #Web scraping de la URL del usuario
-    contenido = Fuente_informacion(website)
+    print(f"Buscando en la web: {website} \n")
+    fuente = Fuente_informacion(website)
+    contenido = fuente.descarga_info()
+
+    #Creo el objeto entropia para guardar un registro en el canal
+    entropia = Entropia(0, 0, [])
+    
+    canal = crea_canal()
+    receptor = Receptor('')
 
     print("Iniciando descarga...\n")
-    Transmisor(contenido)
-    Destino(receptor_1.storage)
+    transmisor = Transmisor(contenido)
+    transmisor.transmite(receptor, canal, entropia)
+
+    #Finalmente muestro al usuario el resultado
+    #Descargar HTML y comparar lo que pesaria completo con lo que pesó finalmente
+    destino = Destino(receptor.storage)
+    #destino.muestra_transmision()
+
+    entropia.calcula_entropia()
 
 main()
